b328ba7ba74a9a0b5fedf039939e34732638f15a
Vladislav-Bezruk <bezrukvladislav@gmail.com>
2020-12-02 17:18:27 +0200

    Добавьте файлы проекта.

---
+++ RavenclawFirstAdditionalPracticeTask/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace RavenclawFirstAdditionalPracticeTask
+{
+    class Program
+    {
+        static void PrintHex(int[] HexNum)
+        {
+            bool t = false;
+
+            for (int i = 3; i >= 0; i--)
+            {
+                if (HexNum[i] > 0)
+                    t = true;
+
+                if (t == true)
+                {
+                    if (HexNum[i] < 10) Console.Write(HexNum[i]);
+                    if (HexNum[i] == 10) Console.Write("A");
+                    if (HexNum[i] == 11) Console.Write("B");
+                    if (HexNum[i] == 12) Console.Write("C");
+                    if (HexNum[i] == 13) Console.Write("D");
+                    if (HexNum[i] == 14) Console.Write("E");
+                    if (HexNum[i] == 15) Console.Write("F");
+                }
+            }
+        }
+
+        static int[] Conv(int Num, int System)
+        {
+            int[] HexNum = new int[4];
+            int i = 0;
+
+            while (Num > 0)
+            {
+                HexNum[i] = Num % System;
+                Num /= System;
+                i++;
+            }
+
+            return HexNum;
+        }
+
+        static int ConvDec(int[] Num, int System)
+        {
+            int NumDec = 0, s = 1;
+
+            for (int i = 0; i < 4; i++)
+            {
+                NumDec += s * Num[i];
+                s *= System;
+            }
+
+            return NumDec;
+        }
+
+        static int[] Sort(int[] Num)
+        {
+            int size = 0, min = 0, imin = 0;
+            int[] NumS = new int[4];
+
+            for (int i = 3; i >= 0; i--)
+                if (Num[i] > 0)
+                {
+                    size = i;
+                    break;
+                }
+
+            for (int j = 0; j <= size; j++)
+            {
+                min = 16;
+                for (int i = size; i >= 0; i--)
+                    if (Num[i] < min)
+                    {
+                        imin = i;
+                        min = Num[i];
+                    }
+                Num[imin] = 17;
+                NumS[j] = min;
+            }
+            return NumS;
+        }
+
+        static void Main(string[] args)
+        {
+            int Num;
+            int[] HexNum = new int[4];
+            bool r = false;
+
+            Console.WriteLine("Enter the number:");
+            Num = Convert.ToInt32(Console.ReadLine());
+            HexNum = Conv(Num, 16);
+
+            Console.Write("1) " + Num + " in Dec = ");
+            PrintHex(HexNum);
+            Console.Write(" in Hex");
+            Console.WriteLine("");
+
+            HexNum = Sort(HexNum);
+
+            Console.Write("2) Sorted " + Num + " in Dec = ");
+            PrintHex(HexNum);
+
+            Console.Write(" in Hex");
+            Console.WriteLine("");
+
+
+            Console.Write("3) ");
+            PrintHex(HexNum);
+
+            Console.Write(" in Hex = " + ConvDec(HexNum, 16) + " in Dec");
+            Console.WriteLine("");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RavenclawFirstPracticeTask/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace RavenclawFirstPracticeTask
+{
+    class Program
+    {
+        static int Maximum(int Pos, int Count, int[] Num)
+        {
+            int MaxCount = 0, Max = -2147483647, PreMax = 0, MMax = 0;
+
+            while (!(MaxCount == Pos || MaxCount >= Count))
+            {
+                Max = -2147483647;
+                for (int i = 0; i < Count; i++)
+                    if (Num[i] > Max && (Num[i] < PreMax || MaxCount == 0))
+                        Max = Num[i];
+                if (MaxCount == 0)
+                    MMax = Max;
+                if (Max == -2147483647)
+                    break;
+                PreMax = Max;
+                MaxCount++;
+            }
+            if (MaxCount == Pos)
+                return Max;
+            else
+                return MMax;
+        }
+
+        static void Main(string[] args)
+        {
+            int Count, Pos;
+
+            Console.WriteLine("Enter the degree of maximum you want to find:");
+            Pos = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number of elements in the array:");
+            Count = Convert.ToInt32(Console.ReadLine());
+            int[] Num = new int[Count];
+
+            for (int i = 0; i < Count; i++)
+            {
+                Console.WriteLine("Enter the " + (i + 1) + " element in the array:");
+                Num[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("The " + Pos + " maximum = " + Maximum(Pos, Count, Num));
+            Console.ReadKey();
+        }
+    }
+}
